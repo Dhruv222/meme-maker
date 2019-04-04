40db9bf2577868947a41f32c1bed1b3ab8640582
Dhruv Sharma <dhrushar@visa.com>
2019-04-04 15:19:25 +0800

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Sharma, Dhruv on 4/4/19.
+//  Copyright © 2019 Sharma, Dhruv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
